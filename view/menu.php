4c31241238cdad5936bbd7a6187d009f5c546549
Anderson Lima <andersoncardosolima0@gmail.com>
2016-10-08 13:06:54 -0300

    criando a pagina home

---
+++ view/menu.php
@@ -0,0 +1,69 @@
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Sistema especialista em Simpósios">
+    <meta name="keywords" content="sgagro, Agronegócios,Faculdade de Ciências Agrárias e Veterinárias, Universidade Estadual Paulista (FCAV/UNESP), FCAV,UNESP,Gestão do Agronegócio,Simpósio em Gestão do Agronegócio, Simpósio, UNESP: Câmpus de Jaboticabal, jaboticabal">
+    <link href="../webroot/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="../webroot/js/bootstrap.min.js"></script>
+  <title>SGAgro</title>
+</head>
+
+<nav class="navbar navbar-default">
+  <div class="row">
+      <div class="col-md-2 col-xs-2">
+          <img src="../webroot/images/logo2.png" style="height: 50%">
+      </div>
+
+      <div class="col-md-8 col-xs-10">
+          <center><h1 style="color: rgb(50,180,74)">II simposio em gestão do agronegocio</h1></center>
+      </div>
+    </div>
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav"   style="color: green;">
+
+        <li class="nav-item"><a class="nav-link" href="home.php">HOME</a></li>
+        <li class="nav-item"><a class="nav-link" href="trabalhos.php">SUBMISSÃO</a></li>
+        <li class="nav-item"><a class="nav-link" href="resultado.php" >RESULTADOS</a></li>
+        <li class="nav-item"><a class="nav-link" href="expediente.php">EXPEDIENTE</a></li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <span>EVENTO</span>
+            <ul class="dropdown-menu">
+              <li class="nav-item"><a class="nav-link" href="evento.php">APRESENTAÇÃO</a></li>
+              <li class="nav-item"><a class="nav-link" href="datasimportantes.php">DATAS IMPORTANTES</a></li>
+              <li class="nav-item"><a class="nav-link" href="programacao.php">PROGRAMAÇÃO</a></li>
+            </ul>
+          </a>
+        </li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <span>EDIÇÕES</span>
+            <ul class="dropdown-menu">
+              <li class="nav-item"><a class="nav-link" href="anais.php">EDIÇÃO ATUAL</a></li>
+              <li class="nav-item"><a class="nav-link" href="edicaoant.php">EDIÇÃO ANTERIORES</a></li> 
+            </ul>
+          </a>
+        </li>
+
+        <li class="nav-item"><a class="nav-link" href="inscricao.php">INSCRIÇÃO</a></li>
+        <li class="nav-item"><a class="nav-link" href="contato.php">CONTATO</a></li>
+        <li class="nav-item"><a class="nav-link" href="../../sistema/index.php" target="blank">SISTEMA</a></li>
+
+      </ul>
+    </div>
+  </nav>
