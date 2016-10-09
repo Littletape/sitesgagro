bfa0f59b862bb86f13bcb95a366eae1883bd6070
kraoolyh <anderson.souza0909@gmail.com>
2016-10-08 21:30:18 -0300

    Criei a tabela de datas importantes

--- view/datasimportantes.php
+++ view/datasimportantes.php
@@ -1,4 +1,54 @@
 <div class="container">
 		<?php include_once("menu.php"); ?>
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<table class="table table-hover table-striped">	
+				<thead>	
+						<th>Período</th>
+						<th>Atividade</th>
+				</thead>
+				<tbody>	
+						<tr>
 
+							<td	>07 de Novembro</td>
+							<td	>Abertura para submissão de trabalhos</td>
+						</tr>
+						<tr>
+
+							<td	></td>
+							<td	>Submissão de Trabalhos</td>
+						</tr>
+						<tr>
+
+							<td	></td>
+							<td	>Encerramento da submissão de Trabalhos</td>
+						</tr>
+						<tr>
+
+							<td	></td>
+							<td	>Divulgação de trabalhos aprovados</td>
+						</tr>
+						<tr>
+
+							<td	></td>
+							<td	>Inscrições com desconto</td>
+						</tr>
+						<tr>
+
+							<td	></td>
+							<td	>Inscrições sem desconto</td>
+						</tr>
+						<tr>
+
+							<td	></td>
+							<td	>Evento</td>
+						</tr>
+				</tbody>
+		</table>
 </div>
+
+<style>
+	table{margin-top: 50px;}
+	th{background-color: #337AB7; color:#fff;}
+	th{text-align: center}
+	.table>tbody>tr>td{text-align: left; padding-left: 150px}
+</style>
