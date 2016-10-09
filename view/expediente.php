5a3ef6fea970b5ee2a358844339c870ce796fba8
kraoolyh <anderson.souza0909@gmail.com>
2016-10-08 22:31:59 -0300

    comissão

--- view/expediente.php
+++ view/expediente.php
@@ -1,4 +1,95 @@
 <div class="container">
 	<?php include_once("menu.php"); ?>
 
+
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<table class="table table-hover table-striped">	
+		<thead>	
+				<th>Título</th>
+				<th>Nome</th>
+				<th>Instituição</th>
+		</thead>
+		<tbody>	
+				<tr>
+					<td>Editora Chefe: Profa. Dra.</td>
+					<td>Lesley Carina do Lago Attadia Galli</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Profa. Dra.</td>
+					<td>Andreia Marize Rodrigues</td>
+					<td>UNESP/FCAV</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>Adriano dos Reis Lucente</td>
+					<td>UNESP/FCAV</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>David Ferreira Lopes Santos</td>
+					<td>UNESP/FCAV</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>Elton Eustaquio Casagrande</td>
+					<td>UNESP/FCLAR</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>Leonardo Fernando Cruz Basso</td>
+					<td>Universidade Mackenzie</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>Mário Sacomano Neto</td>
+					<td>UFSCar</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>Marcelo Girotto Rebelato</td>
+					<td>UNESP/FCAV</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>Roberto Louzada</td>
+					<td>UNESP/FCAV</td>
+				</tr>
+				<tr>
+					<td>Profa. Dra</td>
+					<td>Sheila Farias Alves Garcia</td>
+					<td>UNESP/FCAV</td>
+				</tr>
+				<tr>
+					<td>Prof. Dr.</td>
+					<td>Silvio Carvalho Neto</td>
+					<td>Uni-FACEF</td>
+				</tr>
+		</tbody>	
+		</table>
 	</div>
+
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<table class="table table-hover table-striped" style="margin-top:50px">	
+		<th colspan="2">Informações</th>
+<tr><td class="bold ">Coordenação Geral do Evento:</td><td class="align-left"> Prof. Dr. David Ferreira Lopes Santos – UNESP/FCAV</td></tr>
+
+<tr><td class="bold ">Periodicidade do evento:</td><td class="align-left"> Anual.</td></tr>
+
+<tr><td class="bold ">Idiomas aceitos no evento:</td><td class="align-left"> Português, Espanhol e Inglês.</td></tr>
+
+<tr><td class="bold ">Editora:</td><td class="align-left"> Fundação de Apoio a Pesquisa, Ensino e Extensão - FUNEP</td></tr>
+
+<tr><td class="bold ">Autor Corporativo:</td><td class="align-left"> Faculdade de Ciências Agrárias e Veterinárias, Universidade Estadual Paulista “Júlio de Mesquita Filho”.</td></tr>
+
+<tr><td class="bold ">Via de Acesso:</td><td class="align-left"> Prof. Paulo Donato Castellane, S/N, Vila Industrial, Jaboticabal-SP. CEP: 14.884-900.</td></tr>
+
+</table>
+</div>
+
+<style>
+	th{background-color: #337AB7; color:#fff;}
+	th,td {text-align: center}
+	.bold{font-weight: bold;}
+	.align-left{width:50%; text-align: left;}
+</style>
