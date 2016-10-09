6b6bd99e8ee2fd91d7415f918e3fd53d3be03405
kraoolyh <anderson.souza0909@gmail.com>
2016-10-08 22:30:49 -0300

    tabela de programação

--- view/programacao.php
+++ view/programacao.php
@@ -1,4 +1,32 @@
 <div class="container">
 		<?php include_once("menu.php"); ?>
 
+
+<table class=" table table-bordered table-condensed">
+			<tbody><tr><th>Horario</th><th><a href="#primeiro">Quarta-feira<br>08/06/2017</a></th><th><a href="#segundo">Quinta-feira<br>09/06/2017</a></th><th><a href="#terceiro">Sexta-feira<br>10/06/2017</a></th></tr>
+			<tr><td>09:00</td><td rowspan="5">Credenciamento</td><td rowspan="3">Sessões com apresentações de trabalho</td><td rowspan="3">Mini-Cursos</td></tr>
+			<tr><td>10:00</td></tr>
+			<tr><td>11:00</td></tr>
+			<tr><td>12:00</td><td rowspan="2">Almoço</td><td rowspan="2">Almoço</td></tr>
+			<tr><td>13:00</td></tr>
+			<tr><td>14:00</td><td>Palestra de Abertura</td><td rowspan="5">Palestra:<br> Prof. Dr.Mário Otávio Batalha<br><br> Palestra:<br> Prof. Dr. João Paulo Torres Vieito </td><td rowspan="4">Sessões de Apresentações de Trabalhos</td></tr>
+			<tr><td>15:00</td><td rowspan="4">Mini-cursos</td></tr>
+			<tr><td>16:00</td></tr>
+			<tr><td>17:00</td></tr>
+			<tr><td>18:00</td><td>Encerramento</td></tr>
+			<tr><td>19:00</td><td rowspan="4">Palestra: Me. Marco Antônio Matos – Diretor da ABAG <br><br>Palestra:Prof. Ph.D. Marcus V. Braga Alves
+			</td><td rowspan="4">Mini-cursos</td></tr>
+			<tr><td>20:00</td></tr>
+			<tr><td>21:00</td></tr>
+			<tr><td>22:00</td></tr>
+			
+		</tbody></table>
 </div>
+
+
+<style>
+	table{margin-top: 50px;}
+	th, th>a{background-color: #337AB7; color:#fff;}
+	th, th>a{text-align: center}
+
+</style>
