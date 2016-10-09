47d53ed4716a410c34390db5848589d197739dd2
Marlon <marlu_tcu@hotmail.com>
2016-10-09 09:13:41 -0300

    arquivos do sistema

---
+++ sistema/submissao/cabecalho.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Sistema especialista em Simpósios">
+    <meta name="keywords" content="sgagro, Agronegócios,Faculdade de Ciências Agrárias e Veterinárias, Universidade Estadual Paulista (FCAV/UNESP), FCAV,UNESP,Gestão do Agronegócio,Simpósio em Gestão do Agronegócio, Simpósio, UNESP: Câmpus de Jaboticabal, jaboticabal">
+    <!--<meta charset="utf-8">-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <style type="text/css">
+    	.nav>li>a {
+    font-size: 12px;
+    padding: 6px 5px;
+    font-family:arial,sans-serif;
+    letter-spacing:0px;
+
+    
+}
+    </style>
+
+    <script type="text/javascript">
+    	$(document).on("click","li",function(){
+    		$("li").each(function(){
+    			$(this).removeClass("active");
+    		});
+    		$(this).addClass("active");
+    	});
+    </script>
+   
+	<title>SGAgro</title>
+</head>
+<body>
+	<header>
+		<div class="container-fluid">
+			
+           	<div class="row">
+				<div class="col-md-3 col-md-offset-9" style="font-size: 10px" >
+					<div align=center><b>Número de visualizações do site: </b><img src='http://contador.s12.com.br/img-16364A05-26.gif' border='0' alt='contador de visitantes'><script type='text/javascript' src='http://contador.s12.com.br/ad.js?id=16364A05'></script></div>
+				</div>
+			</div>
+
+	    	<div class="navbar navbar-default">
+    		
+    			<div class="col-md-1">
+		      			<img id="logo" src="images/logo.png" style="width: 160%; height: 160%;" alt="Logo"/>	
+		      	</div>
+		      	<div class="col-md-11">
+		      		<center><h1 style="color: OliveDrab">Simpósio em Gestão do Agronegócio</h1></center>
+		      	</div>
+		      	<ul class="nav navbar-nav navbar-right">
+		      		<li ><a href="home.php" target="iframe_container">HOME</a></li>
+			        <li><a href="evento.php" target="iframe_container">APRESENTAÇÃO</a></li>
+			        <li><a href="datasimportantes.php" target="iframe_container">DATAS IMPORTANTES</a></li>
+			        <li><a href="trabalhos.php" target="iframe_container">SUBMISSÃO</a></li>
+			        <li><a href="resultado.php" target="iframe_container">RESULTADOS</a></li>
+			        <li><a href="programacao.php" target="iframe_container">PROGRAMAÃ‡ÃƒO</a></li>
+			        <li><a href="expediente.php" target="iframe_container">EXPEDIENTE</a></li>
+			        <li><a href="anais.php" target="iframe_container">EDIÇÃO ATUAL</a></li>
+			        <li><a href="edicaoant.php" target="iframe_container">EDIÇÕES ANTERIORES</a></li>
+			        <li><a href="inscricao.php" target="iframe_container">INSCRIÇÃO</a></li>
+			        <!--li><a href="areas.php" target="iframe_container">Ã?REAS</a></li-->
+			        <!--li><a href="goagro.php" target="iframe_container">GOAgro</a></li-->
+			        <li><a href="contato.php" target="iframe_container">CONTATO</a></li>
+		      		<li ><a href="Simposio/submissao/index.php" target="blank">SISTEMA</a></li>
+		      	</ul>
+		      	
+		    </div>
+			    
+			<hr><br><hr>
+        </div>
+	        
+		</div>
+	</header>
+	<section>
