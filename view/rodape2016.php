09b1784c6182adadc668c1be6011234923cae9af
Marlon <marlu_tcu@hotmail.com>
2016-10-12 13:26:39 -0300

    criei um arquivo pro rodape de 2016

---
+++ view/rodape2016.php
@@ -0,0 +1,49 @@
+<div class="col-md-12 col-xs-12" style="background-color: #fff">
+              
+                    <div class="row" style="margin-left:60px">
+                      <div class="col-md-12" >
+                        <div class="col-md-3">
+                          <img src="../webroot/logosrodape/unesplogo.jpg" style="width:35%; heigth:35%;">
+                        </div>
+
+                        <div class="col-md-3">
+                          <img src="../webroot/logosrodape/funeplogo.jpg" style="width:50%; heigth:50%; margin-top:15px">
+                        </div>
+
+                        <div class="col-md-3">
+                          <img src="../webroot/logosrodape/propg.jpg" style="width:30%; heigth:30%;">
+                        </div>
+
+                        <div class="col-md-3">
+                          <img src="../webroot/logosrodape/sociocanalogo.jpg" style="width:50%; heigth:50%; margin-top:25px">
+                        </div>
+                        
+                      </div>
+                    </div>
+                    
+                    <div class="row" style="margin-left:60px;padding-bottom:15px;">
+                      <div class="col-md-12" >
+                        <div class="col-md-3">
+                          <img src="../webroot/logosrodape/sigma.jpg" style="width:50%; heigth:50%;margin-top:20px">
+                        </div>
+
+                                <div class="col-md-3">
+                          <img src="../webroot/logosrodape/Logo_PROEX.jpg" style="width:40%; heigth:40%;margin-top: 10px">
+                        </div>
+
+                        <div class="col-md-3">
+                          <img src="../webroot/logosrodape/marchesan.jpg" style="width:30%; heigth:30%;margin-top: 15px">
+                        </div>
+
+                        <div class="col-md-3">
+                          <img src="../webroot/logosrodape/sicoob.png" style="width:60%; heigth:60%;margin-top: 20px">
+                        </div>
+                      </div> 
+
+                    </div>
+                 
+              </div>
+
+            </div><!--row-->
+
+          </div>
