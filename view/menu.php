c7d66660b0393a47eff03a80076cbd4c8905a305
Anderson Lima <andersoncardosolima0@gmail.com>
2016-10-25 18:33:38 -0200

    Logo modificada como o David pediu

--- view/menu.php
+++ view/menu.php
@@ -50,7 +50,7 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-brand">
       <a href="home.php">
-        <img src="../webroot/images/SGAGRO LOGO.png" width="10%">
+        <img src="../webroot/images/SGAGRO_LOGO.png" width="10%">
       </a>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
