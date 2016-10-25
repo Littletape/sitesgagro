6626a2edd872e094df68980c3fce06fa1ffd146b
Marlon <marlu_tcu@hotmail.com>
2016-10-25 10:05:41 -0200

    alerações

--- view/rodape2017.php
+++ view/rodape2017.php
@@ -1,6 +1,6 @@
  <div id="rodape" class="col-md-12 col-xs-12">
 
-<div class="row" style="margin-top: 50px;text-align: center"><a href="#"><img src="../webroot/images/icoFace.png" width="3%"></a><a href="#"><img src="../webroot/images/icoInsta.png" width="3%"></a><a href="#"><img src="../webroot/images/icoTwit.png" width="3%"></a></div>
+<div class="row" style="margin-top: 50px;text-align: center"><a href="https://www.facebook.com/SGAgro-164350053919134/" target="blank"><img src="../webroot/images/icoFace.png" width="3%"></a><a href="#"><img src="../webroot/images/icoInsta.png" width="3%"></a><a href="#"><img src="../webroot/images/icoTwit.png" width="3%"></a></div>
  <center><h5>Espaço reservado para os patrocionadores</h5></center>
 
 
