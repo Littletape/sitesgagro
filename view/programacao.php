b15c0008edbf30905ec9f003ae64903ce6b89ae3
Anderson Lima <andersoncardosolima0@gmail.com>
2016-12-08 10:43:27 -0200

    Adicionei a programação atualizada

--- view/programacao.php
+++ view/programacao.php
@@ -1,26 +1,30 @@
 <div class="container">
-		<?php include_once("menu.php"); ?>
+	<?php include_once("menu.php"); ?>
 
-<h3 style="text-align: center">Em breve</h3>
-<!-- <table class=" table table-bordered table-condensed">
-			<tbody><tr><th>Horario</th><th><a href="#primeiro">Quarta-feira<br>08/06/2017</a></th><th><a href="#segundo">Quinta-feira<br>09/06/2017</a></th><th><a href="#terceiro">Sexta-feira<br>10/06/2017</a></th></tr>
-			<tr><td>09:00</td><td rowspan="5">Credenciamento</td><td rowspan="3">Sessões com apresentações de trabalho</td><td rowspan="3">Mini-Cursos</td></tr>
+	<!--h3 style="text-align: center">Em breve</h3-->
+	<table class=" table table-bordered table-condensed">
+		<tbody>
+			<tr><th>Horario</th><th><a href="#primeiro">Quarta-feira<br>07/06/2017</a></th><th><a href="#segundo">Quinta-feira<br>08/06/2017</a></th><th><a href="#terceiro">Sexta-feira<br>09/06/2017</a></th></tr>
+			<tr><td>09:00</td><td rowspan="5">Credenciamento</td><td rowspan="3">Sessões com apresentações de trabalho</td><td rowspan="3">Mesa de Discussão</td></tr>
 			<tr><td>10:00</td></tr>
 			<tr><td>11:00</td></tr>
 			<tr><td>12:00</td><td rowspan="2">Almoço</td><td rowspan="2">Almoço</td></tr>
 			<tr><td>13:00</td></tr>
-			<tr><td>14:00</td><td>Palestra de Abertura</td><td rowspan="5">Palestra:<br> Prof. Dr.Mário Otávio Batalha<br><br> Palestra:<br> Prof. Dr. João Paulo Torres Vieito </td><td rowspan="4">Sessões de Apresentações de Trabalhos</td></tr>
-			<tr><td>15:00</td><td rowspan="4">Mini-cursos</td></tr>
+			<tr><td>14:00</td><td>Palestra de Abertura<br><br>Coordenação do Evento</td><td rowspan="5">Palestra 3<br><!--Prof. Dr.Mário Otávio Batalha--><br><br> Palestra 4<br> <!--Prof. Dr. João Paulo Torres Vieito--> </td><td rowspan="5">Sessões de Apresentações de Trabalhos</td></tr>
+			<tr><td>15:00</td><td rowspan="4">Oficinas Tematicas</td></tr>
 			<tr><td>16:00</td></tr>
 			<tr><td>17:00</td></tr>
-			<tr><td>18:00</td><td>Encerramento</td></tr>
-			<tr><td>19:00</td><td rowspan="4">Palestra: Me. Marco Antônio Matos – Diretor da ABAG <br><br>Palestra:Prof. Ph.D. Marcus V. Braga Alves
-			</td><td rowspan="4">Mini-cursos</td></tr>
-			<tr><td>20:00</td></tr>
-			<tr><td>21:00</td></tr>
-			<tr><td>22:00</td></tr>
-			
-		</tbody></table> -->
+			<tr><td>18:00</td></tr>
+			<tr><td rowspan="4">19:00 às 22:30</td><td rowspan="4">Palestra 1 <!--Me. Marco Antônio Matos – Diretor da ABAG--> <br><br>Palestra 2 <!--Prof. Ph.D. Marcus V. Braga Alves-->
+			</td><td rowspan="4">Oficinas Tematicas</td><td rowspan="4">Oficinas Tematicas<br><br><br>Encerramento</td></tr>
+		</tbody>
+	</table><br>
+
+	<p><b>Oficinas Tematicas Confirmadas:</b></p><br>
+	<p><b>Oficina 1: Sustentabilidade na Agricultura</b></p>
+	<p>Profa. Dra. Maria Tereza Noronha - Universidade do Algarve (Portugal)</p><br>
+	<p><b>Oficina 2: Sustentabilidade na Integração Lavoura Pecuária</b></p>
+	<p>Dra. Flavio Fernanda Simili - Agência Paulista de Tecnologia dos Agronegócios (Brasil)</p><br>
 
 </div>
 
